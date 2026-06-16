8867910515998daa6a7b5152593ea5a0e6d063dd
TereshaB <108803022+TereshaB@users.noreply.github.com>
2026-06-17 04:46:13 +0545

    Add: majority-element - submission-5

---
+++ Data Structures & Algorithms/majority-element/submission-5.py
@@ -0,0 +1,53 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # boyer moore : 
+        candidate=nums[0]
+        count = 1 
+        for i in range(1,len(nums)):
+            if(nums[i]==candidate):
+                count+=1
+            elif(count>0):
+                count-=1
+            else : 
+                candidate=nums[i]
+                count=1
+        return candidate
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # tracker={}
+    # for i in nums:
+    #     tracker[i]=tracker.get(i,0)+1
+    # pairs = list(tracker.items())
+    # pairs.sort(key=lambda x:x[1])
+    # result=[]
+    # for i in range (k) :
+    #     list.append(pairs[-(i+1)[0]])
+    # print(list)
