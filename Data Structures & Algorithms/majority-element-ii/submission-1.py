1572bc078332ff1380ad345055c808ee94a94096
TereshaB <108803022+TereshaB@users.noreply.github.com>
2026-06-17 05:11:17 +0545

    Add: majority-element-ii - submission-1

---
+++ Data Structures & Algorithms/majority-element-ii/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        counter ={}
+        ans=[]
+        size =len(nums)
+        counter= defaultdict(int) # avoids key error 
+        for i in nums :
+            counter[i]+=1
+        for key,value in counter.items():
+            if(value>(size//3)):
+                ans.append(key)
+        return ans
+            
+
+        
